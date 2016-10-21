74e6de190215350b11f22210b02c129840dc4c69
LeeHamin <lhm8446@gmail.com>
2016-10-21 18:41:32 +0900

    수식의 후위표기, 연산

--- src/stack/OptExp2.java
+++ src/stack/OptExp2.java
@@ -1,19 +1,21 @@
 package stack;
 
 public class OptExp2 {
-	private String exp;
 	
-	public char[] toPostfix(String infix){
-		char testCh;
-		exp = infix;
-		int expSize = infix.length();
-		int j = 0;
-		char postfix[] = new char[expSize];
+	//수식의 후위표기
+	public String toPostfix(String infix){
+		
+		char temp;
+		
+		String exp = infix;
+		
+		String postfix = "";
+		
 		LinkedStack s = new LinkedStack();
 		
-		for(int i=0; i<expSize; i++){
-			testCh = this.exp.charAt(i);
-			switch(testCh){
+		for(int i=0; i<infix.length(); i++){
+			temp = exp.charAt(i);
+			switch(temp){
 			case '0':
 			case '1':
 			case '2':
@@ -24,44 +26,60 @@ public class OptExp2 {
 			case '7':
 			case '8':
 			case '9':
-				postfix[j++] = testCh; 
+				postfix += temp; 	// 숫자만 스트링에 +
 				break;
 			case '+':
 			case '-':
 			case '*':
 			case '/':
-				s.push(testCh);
+				s.push(temp);		// 연산자만 스택에 push
 				break;
 			case ')':
-				postfix[j++] = s.pop();
+				postfix += s.pop(); // 닫는 괄호를 만날때 연산자 pop
 				break;
 			default:
 			}
 		}
-		postfix[j] = s.pop();
-		return postfix;
+		while(true){				// 스택에 남아있는 연산자 모두 pop
+			if(s.isEmpty()){
+			return postfix;
+			}
+			else{
+				postfix += s.pop();
+			}
+		}
 	}
 	
+	// 후위표기 수식 계산
 	public int evalPostfix(char[] exp){
+		
 		LinkedStack2 s = new LinkedStack2();
+		
 		char[] exp1 = exp;
+		
 		int opr1,opr2,value;
+		
 		char testCh;
 		
-		for(int i = 0; i<7; i++){
+		for(int i = 0; i<exp1.length; i++){
+			
 			testCh = exp1[i];
+			
+			// 연산될 숫자 push
 			if(testCh != '+' && testCh != '-' && testCh != '*' && testCh != '/'){
 				value = testCh - '0';
 				s.push(value);
 			}
+			
+			// 연산자에 따라 숫자 연산 
 			else{
 				opr2 = s.pop();
 				opr1 = s.pop();
 				switch(testCh){
-				case'+' : s.push(opr1 + opr2); break;
-				case'-' : s.push(opr1 - opr2); break;
-				case'*' : s.push(opr1 * opr2); break;
-				case'/' : s.push(opr1 / opr2); break;
+				case'+' : s.push(opr1+opr2); break;
+				case'-' : s.push(opr1-opr2); break;
+				case'*' : s.push(opr1*opr2); break;
+				case'/' : s.push(opr1/opr2); break;
 				}
 			}
 		}
@@ -70,22 +88,25 @@ public class OptExp2 {
 	
 	public static void main(String args[]){
 		OptExp2 opt = new OptExp2();
-		int result ;
 		
-		String exp = "(3*5)-(6/2)";
+		int result =0;
 		
-		char[] exp1 = opt.toPostfix(exp);
+		String ex = "(3*5)-(6/2)*(2-3)";
+		
+		char[] exp1 = opt.toPostfix(ex).toCharArray();
 		
 		result = opt.evalPostfix(exp1);
 		
-		System.out.println("수식  :" + exp);
+		System.out.println("입력된 수식 : " + ex);
+		
 		
 		System.out.print("수식의 후위표기 : ");
 		for(char value : exp1){
 			System.out.print(value);
 		}
 		System.out.println("");
+		
+		
 		System.out.println("연산 결과  : " + result);
-
 	}
 }
